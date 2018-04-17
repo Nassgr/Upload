fee85e665a98188d88318c26b24f3948d4dcc878
Nassgr <anas.grina@hotmail.fr>
2018-04-17 14:16:37 +0200

    correction bug

--- index.php
+++ index.php
@@ -45,10 +45,10 @@ if(!empty($_FILES)) {
 $directory = 'upload/';
 $listUpload = new FilesystemIterator($directory);
 
-if (file_exists("upload/" . $_POST['delete'])) {
-    unlink("upload/" . $_POST['delete']);
-    header('Location: index.php');
-}
+//if (file_exists("upload/" . $_POST['delete'])) {
+  //  unlink("upload/" . $_POST['delete']);
+    //header('Location: index.php');}
+
 
 ?>
 
